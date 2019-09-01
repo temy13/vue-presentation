7dd03b5d2d10e058bc8064e8d7ee8861f4b719c4
temy13 <temy.poporofier00@gmail.com>
2019-09-01 11:10:42 +0900

    add plugins

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,10 +20,10 @@
     v-container.height100.grey.lighten-5
       h1 input
       v-row#input.height100(no-gutters='')
-        v-col.height100(:key='0')
+        v-col.height100(:key='0' :cols="3")
           v-card.height100.pa-2(tile='' outlined='')
             v-textarea.height100(rows="30" v-model="markdown")
-        v-col#review.height100(:key='1')
+        v-col#review.height100(:key='1' :cols="9")
           v-card.height100.pa-2(tile='' outlined='')
             mark-display(:markdown='markdown' :isFull="false" @title='setTitle' supportPreview='' keyboardCtrl='' autoFontSize='' )
 </template>
@@ -31,26 +31,39 @@
 import MarkDisplay from 'vue-mark-display'
 import marked from 'marked'
 
+// const markdown = `
+// <!-- color: red; -->
+// <!-- style: font-weight: bold; -->
+//
+// # This is First Slide
+// Self intro here.
+//
+// ----
+//
+// <!-- stageBackground: silver -->
+//
+// ## The Second Slide
+// - list item 1
+// - list item 2
+// - list item 3
+//
+// ----
+//
+// # The END
+// Thanks.
+// `
 const markdown = `
-<!-- color: red; -->
-<!-- style: font-weight: bold; -->
 
-# This is First Slide
-Self intro here.
+\`\`\`horizontalAxis
+- アカデミック
+- 蒸留
+- AI/ML(text clf)
+- 開発(ML APIの組み込み)
+- インタビューからの企画
+- ビジネス
+\`\`\`
 
-----
 
-<!-- stageBackground: silver -->
-
-## The Second Slide
-- list item 1
-- list item 2
-- list item 3
-
-----
-
-# The END
-Thanks.
 `
 
 export default {
@@ -71,7 +84,7 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped" attribute to limit CSS to this component only  scoped lang="scss"-->
 <style>
 body {
   margin: 0;
@@ -98,5 +111,26 @@ body {
 }
 #review .slide-content{
   height: 100%;
+}
+.horizontalAxis{
+  width: 100%;
+  position: relative;
+}
+.horizontalAxis li{
+  list-style-type: none;
+  float: left;
+  width: 100px;
+  text-align: center;
+  font-size:20px;
+}
+.horizontalAxis li:first-child{
+  position: absolute;
+  left: 0;
+  bottom: -20px;
+}
+.horizontalAxis li:last-child{
+  position: absolute;
+  right: 0;
+  bottom: -20px;
 }
 </style>
